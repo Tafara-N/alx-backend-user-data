36f561f915751f38f111479529f32782dce19b2f
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-09 00:07:32 +0200

    User session.

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+User session module
+"""
+
+from models.base import Base
+
+
+class UserSession(Base):
+    """
+    User session class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """
+        Initializing a User session instance
+        """
+        
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
