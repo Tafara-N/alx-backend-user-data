5f83e4b4678a0b47e90209101b4b8f0c81cd9d4f
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-09 00:14:09 +0200

    Session authentication.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -87,6 +87,7 @@ def authenticate_user():
                 abort(403)
             request.current_user = user
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", 5000)
