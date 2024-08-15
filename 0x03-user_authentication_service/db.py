5a9aaf4e5dd8967988370c00e7bce8beceb58e63
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-16 00:51:53 +0200

    User authentication service.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-
+"""DB module
 """
-DB module
-"""
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -15,26 +12,18 @@ from user import Base, User
 
 
 class DB:
-    """
-    DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """
-        Initialize a new DB instance
-        """
-
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """
-        Memoized session object
-        """
-
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
