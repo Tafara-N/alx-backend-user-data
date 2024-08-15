f64a26e51d671700534635c1b2a86c77b39ea416
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-16 00:50:06 +0200

    A class named `User` that represents a table named `users` in a database.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -21,5 +21,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
