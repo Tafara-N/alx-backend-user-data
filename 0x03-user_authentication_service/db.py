c960cdfdbe8f07bab8d6d5e53932da4de7591615
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-15 15:53:50 +0200

    User authentication service.

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+DB module
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """
+    DB class
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Saving the user to the database
+        """
+
+        if not email or not hashed_password:
+            return
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
