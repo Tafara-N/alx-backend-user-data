b1e4e562b8259bbaf8a0446b7dbfc6cbba2e6f9a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-15 16:02:22 +0200

    User authentication service.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,14 +42,16 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Saving the user to the database
+        Saving a new user to the database
         """
 
-        if not email or not hashed_password:
-            return
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            self._session.rollback()
+        except InvalidRequestError:
+            user = None
         return user
 
     def find_user_by(self, **kwargs) -> User:
