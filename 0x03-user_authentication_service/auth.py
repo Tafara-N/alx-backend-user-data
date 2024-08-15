162b20ce341baa81ffad332a0c87d806a3a78522
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-15 17:14:08 +0200

    Authentication related routines.

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+User authentication service
+"""
+
+import bcrypt
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> str:
+    """
+    Hash a password
+
+    Parameters
+        password - the password to be hashed
+
+    Return
+        The hashed password
+    """
+
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+
+def _generate_uuid() -> str:
+    """
+    Generating a UUID
+    """
+
+    return str(uuid4())
