1319020cb88df8b4f0eb22cda567ded75c3759a8
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-15 17:27:05 +0200

    User authentication service.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,12 +5,11 @@ DB module
 """
 
 from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from sqlalchemy.orm.session import Session
 from user import Base, User
 
 
