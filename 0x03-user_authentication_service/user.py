2a8a3c83210f992764a733c4cf067f9a909e76dd
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-15 15:43:26 +0200

    A class named `User` that represents a table named `users` in a database.

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+A model named `User` for a database table named `users` by using the
+mapping declaration
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    A class named `User` that represents a table named `users` in a database
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
